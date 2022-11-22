af89e50abeae97182e13a061a1d24785b78d6e69
Janusz Czornik <84579675+jczornik@users.noreply.github.com>
2022-11-23 00:44:56 +0100

    Add support for png flags (#1)

--- png_reader.h
+++ png_reader.h
@@ -13,7 +13,14 @@ struct chunk_t {
   uint8_t *type;
   uint8_t *data;
   uint8_t *crc;
+
+  uint8_t flags;
 };
+
+enum ancillary_flag { CRITICAL = 0, ANCILLARY };
+enum private_flag { PUBLIC = 0, PRIVATE };
+enum reserved_flag { CORRECT = 0, ERROR };
+enum safe_to_copy_flag { UNSAFE, SAFE };
 
 bool check_magic_number(FILE *img_file);
 
@@ -21,6 +28,14 @@ void free_chunk_fields(struct chunk_t *chunk);
 
 void free_chunk(struct chunk_t **chunk);
 
-struct chunk_t* read_chunk(FILE *img_file);
+struct chunk_t *read_chunk(FILE *img_file);
+
+enum ancillary_flag get_ancillary_flag(const struct chunk_t *chunk);
+
+enum private_flag get_private_flag(const struct chunk_t *chunk);
+
+enum reserved_flag get_reserved_flag(const struct chunk_t *chunk);
+
+enum safe_to_copy_flag get_safe_to_copy_flag(const struct chunk_t *chunk);
 
 #endif
